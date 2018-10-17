4b6bddfa678440833093c7e7109cdf5491ea9c4e
Bruno Eduardo <brunoeduardocoe@gmail.com>
2018-10-17 12:49:23 -0300

    Implementação do exemplo'

--- js/exemplo.js
+++ js/exemplo.js
@@ -29,3 +29,40 @@ var comic = {person: 'Comic guy', hairLength: 8, weight: 290, age: 38};
 
 var decisionTreePrediction = decisionTree.predict(comic);
 var randomForestPrediction = randomForest.predict(comic);
+
+
+// Displaying Decision Tree
+document.getElementById('displayTree').innerHTML = treeToHtml(decisionTree.root);
+
+
+// Recursive (DFS) function for displaying inner structure of decision tree
+function treeToHtml(tree) {
+    // only leafs containing category
+    if (tree.category) {
+        return  ['<ul>',
+                    '<li>',
+                        '<a href="#">',
+                            '<b>', tree.category, '</b>',
+                        '</a>',
+                    '</li>',
+                 '</ul>'].join('');
+    }
+    
+    return  ['<ul>',
+                '<li>',
+                    '<a href="#">',
+                        '<b>', tree.attribute, ' ', tree.predicateName, ' ', tree.pivot, ' ?</b>',
+                    '</a>',
+                    '<ul>',
+                        '<li>',
+                            '<a href="#">yes</a>',
+                            treeToHtml(tree.match),
+                        '</li>',
+                        '<li>', 
+                            '<a href="#">no</a>',
+                            treeToHtml(tree.notMatch),
+                        '</li>',
+                    '</ul>',
+                '</li>',
+             '</ul>'].join('');
+}
