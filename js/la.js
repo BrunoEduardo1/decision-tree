f2cab7da453c1909d92ec3ed454f5d0d323ad1a5
Bruno Eduardo <brunoeduardocoe@gmail.com>
2018-10-18 16:25:06 -0300

    O risco pode ser string

--- js/la.js
+++ js/la.js
@@ -10,7 +10,7 @@
 //Training set
 var data = 
     [
-    {historic: 1,  debt: 0,  guarantee: 1, revenue: 1500,  risc: 2},
+    {historic: 1,  debt: 0,  guarantee: 1, revenue: 1500,  risc: 2},//o risco pode ser string
     {historic: 0,  debt: 2,  guarantee: 0, revenue: 2500,  risc: 2},
     {historic: 0,  debt: 1,  guarantee: 0, revenue: 2500,  risc: 1},
     {historic: 0,  debt: 1,  guarantee: 0, revenue: 4000,  risc: 2},
@@ -25,12 +25,12 @@ var data =
     {historic: 2,  debt: 2,  guarantee: 0, revenue: 4000,  risc: 0},
     {historic: 1,  debt: 2,  guarantee: 0, revenue: 2500,  risc: 2}
     ];
-
+    
      // Configuration
     var config = {
         trainingSet: data, 
         categoryAttr: 'risc', //maybe this one could be risc
-        //ignoredAttributes: ['person']
+        //ignoredAttributes: ['nop']
     };
 
 
