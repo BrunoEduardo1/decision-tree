d27ed3292b91d04be967759664fb571a443b919b
Bruno Eduardo <brunoeduardocoe@gmail.com>
2018-10-18 15:12:25 -0300

    Dados para o treinamento inseridos Decidir qual será o atributo categoria, por agora será o risco.

--- js/la.js
+++ js/la.js
@@ -9,12 +9,32 @@
 
 */
 var data = 
-    [{person: 'Homer',  hairLength: 0,  weight: 250, age: 36, sex: 'male'},
-     {person: 'Marge',  hairLength: 10, weight: 150, age: 34, sex: 'female'},
-     {person: 'Bart',   hairLength: 2,  weight: 90,  age: 10, sex: 'male'},
-     {person: 'Lisa',   hairLength: 6,  weight: 78,  age: 8, sex: 'female'},
-     {person: 'Maggie', hairLength: 4,  weight: 20,  age: 1, sex: 'female'},
-     {person: 'Abe',    hairLength: 1,  weight: 170, age: 70, sex: 'male'},
-     {person: 'Selma',  hairLength: 8,  weight: 160, age: 41, sex: 'female'},
-     {person: 'Otto',   hairLength: 10, weight: 180, age: 38, sex: 'male'},
-     {person: 'Krusty', hairLength: 6,  weight: 200, age: 45, sex: 'male'}];
+    [
+    {historic: 1,  debt: 0,  guarantee: 1, revenue: '<15',      risc: 2},
+    {historic: 0,  debt: 2,  guarantee: 0, revenue: '>15 <35',  risc: 2},
+    {historic: 0,  debt: 1,  guarantee: 0, revenue: '>15 <35',  risc: 1},
+    {historic: 0,  debt: 1,  guarantee: 0, revenue: '>35',      risc: 2},
+    {historic: 0,  debt: 1,  guarantee: 0, revenue: '>35',      risc: 0},
+    {historic: 0,  debt: 1,  guarantee: 1, revenue: '>35',      risc: 0},
+    {historic: 1,  debt: 1,  guarantee: 0, revenue: '<15',      risc: 2},
+    {historic: 1,  debt: 1,  guarantee: 1, revenue: '>35',      risc: 1},
+    {historic: 2,  debt: 1,  guarantee: 0, revenue: '>35',      risc: 0},
+    {historic: 2,  debt: 2,  guarantee: 1, revenue: '>35',      risc: 0},
+    {historic: 2,  debt: 2,  guarantee: 0, revenue: '<15',      risc: 2},
+    {historic: 2,  debt: 2,  guarantee: 0, revenue: '>15 <35',  risc: 1},
+    {historic: 2,  debt: 2,  guarantee: 0, revenue: '>35',      risc: 0},
+    {historic: 1,  debt: 2,  guarantee: 0, revenue: '>15 <35',  risc: 2}
+    ];
+
+     // Configuration
+    var config = {
+        trainingSet: data, 
+        categoryAttr: 'risc', //maybe this one could be risc
+        //ignoredAttributes: ['person']
+    };
+
+
+    // Building Decision Tree
+    var decisionTree = new dt.DecisionTree(config);
+
+    console.log(decisionTree);
